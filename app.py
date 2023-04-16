e842fecb4ee23024f5d5ff4d1793fdc93c1ccb92
danielfogel <130784081+danielfogel@users.noreply.github.com>
2023-04-16 19:12:16 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
+import mysql.connector
+import requests
+import xlsxwriter
+from scrapy.selector import Selector
+from flaskext.mysql import MySQL
+import urllib.request
+import time
+from datetime import datetime
+
+import requests
+app = Flask(__name__)
+
+
+CORS(app)
+
+@app.route("/<path:path>")
+def index(path=None):
+     
+     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+     response = requests.get(request.url.rstrip("?")[22:], headers=headers)
+     
+     print(response.content)
+     h = []
+     p = []
+     pt = []
+     products = []
+     sel = Selector(text=response.content)
+     
+     heads = sel.xpath("//*[@class='s-item__wrapper clearfix']")
+
+     for head in heads:
+        h=head.xpath(".//*[@class='s-item__title']/span/text()").extract_first()
+        p=head.xpath(".//*[@class='s-item__price']//text()").extract_first()
+        pt=head.xpath(".//*[@class='s-item__image-wrapper image-treatment']/img/@src").extract_first()
+        products.append({"head": h, "price": p, "image":pt})
+     return jsonify(products)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
